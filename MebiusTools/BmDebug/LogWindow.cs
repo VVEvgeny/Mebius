ecb8dab053fdbe13854e42b4d7bb874401eb9e20
Evgeny <vvevgeny@gmail.com>
2016-09-13 16:03:17 +0300

    end day

--- MebiusTools/BmDebug/LogWindow.cs
+++ MebiusTools/BmDebug/LogWindow.cs
@@ -5,7 +5,7 @@ namespace BmDebug
 {
     public partial class LogWindow : Form
     {
-        private RichTextBox _richTextBox1;
+        private RichTextBox richTextBoxText;
     
         public LogWindow()
         {
@@ -14,25 +14,36 @@ namespace BmDebug
         public void Write_Line(string text)
         {
             if (!Visible) Visible = true;
-            _richTextBox1.Text += text + Environment.NewLine;
+            richTextBoxText.Text += text + Environment.NewLine;
         }
 
         private void InitializeComponent()
         {
-            _richTextBox1 = new RichTextBox();
-            SuspendLayout();
+            this.richTextBoxText = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBoxText
+            // 
+            this.richTextBoxText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxText.Location = new System.Drawing.Point(0, 0);
+            this.richTextBoxText.Name = "richTextBoxText";
+            this.richTextBoxText.Size = new System.Drawing.Size(808, 210);
+            this.richTextBoxText.TabIndex = 0;
+            this.richTextBoxText.Text = "";
+            this.richTextBoxText.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.richTextBoxText_MouseDoubleClick);
+            // 
+            // LogWindow
+            // 
+            this.ClientSize = new System.Drawing.Size(808, 210);
+            this.Controls.Add(this.richTextBoxText);
+            this.Name = "LogWindow";
+            this.ResumeLayout(false);
 
-            _richTextBox1.Dock = DockStyle.Fill;
-            _richTextBox1.Location = new System.Drawing.Point(0, 0);
-            _richTextBox1.Name = "_richTextBox1";
-            _richTextBox1.Size = new System.Drawing.Size(808, 210);
-            _richTextBox1.TabIndex = 0;
-            _richTextBox1.Text = "";
+        }
 
-            ClientSize = new System.Drawing.Size(808, 210);
-            Controls.Add(_richTextBox1);
-            Name = "LogWindow";
-            ResumeLayout(false);
+        private void richTextBoxText_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            richTextBoxText.Text = string.Empty;
         }
     }
 }

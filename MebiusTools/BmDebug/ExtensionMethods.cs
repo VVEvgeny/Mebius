004bd8157b62b82d970579f167efbcdca6c8502d
Evgeny <vvevgeny@gmail.com>
2016-09-26 09:22:35 +0300

    To Debug extension

---
+++ MebiusTools/BmDebug/ExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿namespace BMTools
+{
+    public static class BmDebugExtension
+    {
+        public static object ToDebug(this object obj)
+        {
+            BmDebug.Info(obj);
+            return obj;
+        }
+    }
+}

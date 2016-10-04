563123fd524ed95c13c96af5d869b18030f37c32
Evgeny <vvevgeny@gmail.com>
2016-10-04 10:36:20 +0300

    +PerformanceTest

---
+++ MebiusTools/PerformanceTest/OperationTimer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PerformanceTest
+{
+    sealed class OperationTimer: IDisposable
+    {
+        private readonly Stopwatch _startTime;
+        private readonly string _text;
+        private readonly int _collectionCount;
+
+        public OperationTimer(string text)
+        {
+            Prepare();
+
+            _text = text;
+            _collectionCount = GC.CollectionCount(0);
+            _startTime = Stopwatch.StartNew();
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"time={_startTime.Elapsed} GC={_collectionCount} text={_text}");
+        }
+
+        private static void Prepare()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+    }
+}
---
+++ MebiusTools/PerformanceTest/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace PerformanceTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            var iterations = 100;
+
+            BMTools.BmDebug.ClassUsing = "test1";
+            
+            BMTools.BmDebug.Enabled = false;
+            BMTools.BmDebug.DebugOutput = BMTools.BmDebug.DebugOutputModes.File;
+            for (var i = 0; i < iterations; i++)//прогрев
+            {
+                BMTools.BmDebug.Info("test");
+            }
+            using (new OperationTimer("debug Disabled call reference file"))
+            {
+                for (var i = 0; i < iterations; i++)
+                {
+                    BMTools.BmDebug.Info("test");
+                }
+            }
+            using (new OperationTimer("debug Disabled call value file"))
+            {
+                for (var i = 0; i < iterations; i++)
+                {
+                    BMTools.BmDebug.Info(105);
+                }
+            }
+
+            BMTools.BmDebug.DebugOutput = BMTools.BmDebug.DebugOutputModes.LogWindow;
+            for (var i = 0; i < iterations; i++)//прогрев
+            {
+                BMTools.BmDebug.Info("test");
+            }
+            using (new OperationTimer("debug Disabled call reference log window"))
+            {
+                for (var i = 0; i < iterations; i++)
+                {
+                    BMTools.BmDebug.Info("test");
+                }
+            }
+            using (new OperationTimer("debug Disabled call value log window"))
+            {
+                for (var i = 0; i < iterations; i++)
+                {
+                    BMTools.BmDebug.Info(105);
+                }
+            }
+
+            BMTools.BmDebug.Enabled = true;
+            BMTools.BmDebug.DebugOutput = BMTools.BmDebug.DebugOutputModes.File;
+            for (var i = 0; i < iterations; i++)//прогрев
+            {
+                BMTools.BmDebug.Info("test");
+            }
+            using (new OperationTimer("debug Enabled call reference file"))
+            {
+                for (var i = 0; i < iterations; i++)
+                {
+                    BMTools.BmDebug.Info("test");
+                }
+            }
+            using (new OperationTimer("debug Enabled call value file"))
+            {
+                for (var i = 0; i < iterations; i++)
+                {
+                    BMTools.BmDebug.Info(105);
+                }
+            }
+
+            BMTools.BmDebug.DebugOutput = BMTools.BmDebug.DebugOutputModes.LogWindow;
+            for (var i = 0; i < iterations; i++)//прогрев
+            {
+                BMTools.BmDebug.Info("test");
+            }
+            using (new OperationTimer("debug Enabled call reference log window"))
+            {
+                for (var i = 0; i < iterations; i++)
+                {
+                    BMTools.BmDebug.Info("test");
+                }
+            }
+            using (new OperationTimer("debug Enabled call value log window"))
+            {
+                for (var i = 0; i < iterations; i++)
+                {
+                    BMTools.BmDebug.Info(105);
+                }
+            }
+        }
+    }
+}

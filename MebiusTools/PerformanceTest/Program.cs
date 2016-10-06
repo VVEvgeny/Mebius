e95f36ec8070c9fa309467fd2a7f48c81820b0fb
Evgeny <vvevgeny@gmail.com>
2016-10-06 17:16:28 +0300

    test

--- MebiusTools/PerformanceTest/Program.cs
+++ MebiusTools/PerformanceTest/Program.cs
@@ -1,13 +1,57 @@
-﻿namespace PerformanceTest
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace PerformanceTest
 {
     class Program
     {
         static void Main()
         {
+            using (new OperationTimer("generic List value type"))
+            {
+                var iterations = 100000;
+                List<int> d= new List<int>(iterations);
+                for (int i = 0; i < iterations; i++)
+                {
+                    d.Add(i);
+                }
+            }
+            using (new OperationTimer("generic List ref type"))
+            {
+                var iterations = 100000;
+                List<string> d = new List<string>(iterations);
+                for (int i = 0; i < iterations; i++)
+                {
+                    d.Add(i.ToString());
+                }
+            }
+
+            using (new OperationTimer("not generic List value type"))
+            {
+                var iterations = 100000;
+                ArrayList al =   new ArrayList();
+
+                for (int i = 0; i < iterations; i++)
+                {
+                    al.Add(i);
+                }
+            }
+            using (new OperationTimer("not generic List ref type"))
+            {
+                var iterations = 100000;
+                ArrayList al = new ArrayList();
+
+                for (int i = 0; i < iterations; i++)
+                {
+                    al.Add(i.ToString());
+                }
+            }
+
+            /*
             var iterations = 100;
 
             BMTools.BmDebug.ClassUsing = "test1";
-            
+ 
             BMTools.BmDebug.Enabled = false;
             BMTools.BmDebug.DebugOutput = BMTools.BmDebug.DebugOutputModes.File;
             for (var i = 0; i < iterations; i++)//прогрев
@@ -89,6 +133,7 @@
                     BMTools.BmDebug.Info(105);
                 }
             }
+            */
         }
     }
 }
